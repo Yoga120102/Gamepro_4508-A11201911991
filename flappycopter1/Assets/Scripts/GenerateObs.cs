b8dd5fcb2d1868ab8669b83ff8e60540fe2877a2
Yoga Indratama <yogaindratama158@gmail.com>
2021-12-13 20:43:31 +0700

    Eleventh Commit

---
+++ flappycopter1/Assets/Scripts/GenerateObs.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObs : MonoBehaviour
+{
+    public GameObject rocks;
+    public int score=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("CreateObstacle", 1f, 1.5f);
+    }
+    void CreateObstacle()
+    {
+        Instantiate (rocks);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
